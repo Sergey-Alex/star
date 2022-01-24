a029f921b82bce107a0aa6e755c70e16726bbc55
Sergey-Alex <stroitelevsergeyalex@gmail.com>
2022-01-24 19:46:09 +0300

    del idea

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import {FaStar} from 'react-icons/fa'
+import React, {useState} from 'react';
+import Star from "./Star";
 
-const StarRating = () => {
-    return [
-        <FaStar color ='red'/>,
-        <FaStar color ='red'/>,
-        <FaStar color ='red'/>,
-        <FaStar color ='grey'/>,
-        <FaStar color ='grey'/>,
+const StarRating = ({totalStars = 5}) => {
+    const createArray = length => [...Array(length)]
 
-    ];
+    const [selectedStar,setSelectedStar] = useState(0)
+
+    return (
+        <>
+            {createArray(totalStars).map((n, i) =>
+                <Star
+                    key ={i}
+                    selected={selectedStar > i}
+                    onSelect={() =>setSelectedStar(i+1)}
+                />
+             )}
+             <p>{selectedStar} of {totalStars}</p>
+        </>
+
+    )
+
 };
 
 export default StarRating;
