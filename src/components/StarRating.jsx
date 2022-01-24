ead617f80d84a17d62fe64d2e5c7f5ea0a61f4da
Sergey-Alex <stroitelevsergeyalex@gmail.com>
2022-01-24 19:44:17 +0300

    first commit

---
+++ src/components/StarRating.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {FaStar} from 'react-icons/fa'
+
+const StarRating = () => {
+    return [
+        <FaStar color ='red'/>,
+        <FaStar color ='red'/>,
+        <FaStar color ='red'/>,
+        <FaStar color ='grey'/>,
+        <FaStar color ='grey'/>,
+
+    ];
+};
+
+export default StarRating;
